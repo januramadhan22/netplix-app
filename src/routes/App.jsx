71600c6db2d66abb0cf4b70e691f5d711c247e00
Janu <abbyjunior600@gmail.com>
2023-02-09 01:44:20 +0700

    fix auth with firebase

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import HomePage from "../pages/HomePage";
 import LandingPage from "../pages/LandingPage";
+import { onAuthStateChanged } from "firebase/auth";
+import { userAuth } from "../utils/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "../utils/userSlice/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(userAuth, (user) => {
+      if (user) {
+        dispatch(
+          login({
+            uid: user.uid,
+            email: user.email,
+          })
+        );
+      } else {
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <BrowserRouter>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { auth, getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDJOnxp2aX9pEu_qD4g9Df3ikNgQAkwgbQ",
   authDomain: "netplix-app-f9e12.firebaseapp.com",
@@ -6,3 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "644922867433",
   appId: "1:644922867433:web:a5f306db1c7716356b3a78",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+
+export const userAuth = getAuth(firebaseApp);
