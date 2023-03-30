704dc506d73ef5805b8db6bf232671429f690dbd
Janu <abbyjunior600@gmail.com>
2023-03-30 10:00:03 +0700

    create loading component for detail

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -5,16 +5,20 @@ import { API_KEY } from "../utils/apiKey";
 import TopNav from "../components/TopNav";
 import { addFavorite } from "../utils/favoriteSlice/favoriteSlice";
 import { useDispatch } from "react-redux";
+import DetailLoading from "../components/card/DetailLoading";
 
-function DetailPage(props) {
+function DetailPage() {
   const { uid } = useParams();
   const [detail, setDetail] = useState({});
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchDetail = async () => {
+      setLoading(true);
       const request = await axios.get(`/movie/${uid}?api_key=${API_KEY}`);
       setDetail(request.data);
+      setLoading(false);
       return request;
     };
 
@@ -52,38 +56,43 @@ function DetailPage(props) {
       <TopNav />
 
       {/* Detail Content */}
-      <div className="relative z-30 w-10/12 p-12 mt-10 mx-auto bg-white bg-opacity-20 rounded-md flex flex-col md:flex-row gap-3 md:gap-10">
-        <img
-          src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
-          alt={detail.title}
-          className="max-h-96 rounded"
-        />
-        <div className="w-full flex flex-col items-start gap-2 text-center md:text-left text-white">
-          <h1 className="text-4xl font-medium">{detail.title}</h1>
-          <p className="w-full font-medium">
-            Duration: <span className="font-normal">{detail.runtime}min</span>
-          </p>
-          <p className="w-full font-medium">
-            Release: <span className="font-normal">{detail.release_date}</span>
-          </p>
-          <p className="w-full font-medium">
-            Genre:{" "}
-            <span className="font-normal">
-              {detail.genres?.map((list) => list.name + ", ")}
-            </span>
-          </p>
-          <p className="w-full font-medium">
-            Overview: <br />
-            <span className="font-normal">{detail.overview}</span>
-          </p>
-          <button
-            onClick={() => handleAddFavorite(detail)}
-            className="mt-4 mx-auto md:mx-0 px-8 py-2 text-sm font-medium bg-red-700 rounded"
-          >
-            Add To Playlist
-          </button>
+      {loading ? (
+        <DetailLoading />
+      ) : (
+        <div className="relative z-30 w-10/12 p-12 mt-10 mx-auto bg-white bg-opacity-20 rounded-md flex flex-col md:flex-row gap-3 md:gap-10">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+            alt={detail.title}
+            className="max-h-96 rounded"
+          />
+          <div className="w-full flex flex-col items-start gap-2 text-center md:text-left text-white">
+            <h1 className="w-full text-4xl font-medium">{detail.title}</h1>
+            <p className="w-full font-medium">
+              Duration: <span className="font-normal">{detail.runtime}min</span>
+            </p>
+            <p className="w-full font-medium">
+              Release:{" "}
+              <span className="font-normal">{detail.release_date}</span>
+            </p>
+            <p className="w-full font-medium">
+              Genre:{" "}
+              <span className="font-normal">
+                {detail.genres?.map((list) => list.name + ", ")}
+              </span>
+            </p>
+            <p className="w-full font-medium">
+              Overview: <br />
+              <span className="font-normal">{detail.overview}</span>
+            </p>
+            <button
+              onClick={() => handleAddFavorite(detail)}
+              className="mt-4 mx-auto md:mx-0 px-8 py-2 text-sm font-medium bg-red-700 rounded"
+            >
+              Add To Playlist
+            </button>
+          </div>
         </div>
-      </div>
+      )}
 
       {/* Overlay */}
       <div className="absolute top-0 w-full h-full z-10 bg-black bg-opacity-60 customGradient" />
